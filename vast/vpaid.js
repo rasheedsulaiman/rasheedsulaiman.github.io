b67e8df0e577907b9c9b36f36e2334804aeecd23
rasheedsulaiman <rasheedsulaiman@users.noreply.github.com>
2024-09-16 19:36:55 -0400

    Update vpaid.js

--- vast/vpaid.js
+++ vast/vpaid.js
@@ -11,31 +11,28 @@
       this.callEvent('AdLoaded');
   };
 
-  // Inject the HTML and script into the video player
+  // Inject the entire HTML + script code as-is
   VPAIDAd.prototype.injectAdHtml = function() {
-      // Create a container for the HTML
       var container = document.createElement('div');
-      container.innerHTML = '<div id="ld-7573-2519"></div>';
 
-      // Append the container to the body or video player
+      // Insert the HTML as-is, keeping it intact
+      container.innerHTML = `<div id="ld-7573-2519"></div>
+          <script>
+              (function(w,d,s,i){
+                  w.ldAdInit=w.ldAdInit||[];
+                  w.ldAdInit.push({slot:13217938886688870,size:[0, 0],id:"ld-7573-2519"});
+                  if(!d.getElementById(i)){
+                      var j=d.createElement(s),p=d.getElementsByTagName(s)[0];
+                      j.async=true;
+                      j.src="https://testing.test.decide.co/_js/ajs.js";
+                      j.id=i;
+                      p.parentNode.insertBefore(j,p);
+                  }
+              })(window,document,"script","ld-ajs");
+          </script>`;
+
+      // Append the container to the document body
       document.body.appendChild(container);
-
-      // Load the external JavaScript
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.src = 'https://testing.test.decide.co/_js/ajs.js';
-
-      // Append the script to the body
-      document.body.appendChild(script);
-
-      // Trigger the ad display (optional)
-      window.ldAdInit = window.ldAdInit || [];
-      window.ldAdInit.push({
-          slot: 13217938886688870,
-          size: [0, 0],
-          id: "ld-7573-2519"
-      });
   };
 
   VPAIDAd.prototype.startAd = function() {
