f2bcd86aee0465b23b9178ac456cf84fab61f43f
rasheedsulaiman <rasheedsulaiman@users.noreply.github.com>
2024-09-18 17:16:28 -0400

    Update vpaid-creative.js

--- copilot/vpaid-creative.js
+++ copilot/vpaid-creative.js
@@ -7,9 +7,10 @@ var VPAIDCreative = function() {
     adExpanded: false,
     adSkippableState: false,
     adRemainingTime: 0,
-    adDuration: 30,
+    adDuration: 0,
     adVolume: 1.0
   };
+  this.videoElement = null;
 };
 
 VPAIDCreative.prototype.handshakeVersion = function(version, callback) {
@@ -20,16 +21,34 @@ VPAIDCreative.prototype.initAd = function(width, height, viewMode, desiredBitrat
   this.attributes.adWidth = width;
   this.attributes.adHeight = height;
   this.attributes.adDuration = 30; // Example duration
+
+  // Create video element
+  this.videoElement = document.createElement('video');
+  this.videoElement.width = width;
+  this.videoElement.height = height;
+  this.videoElement.src = 'https://rasheedsulaiman.github.io/vast/test-ad.mp4'; // Replace with your ad video URL
+  this.videoElement.controls = true;
+
+  // Append video element to the body
+  document.body.appendChild(this.videoElement);
+
   this.dispatchEvent('AdLoaded');
   callback();
 };
 
 VPAIDCreative.prototype.startAd = function(callback) {
+  if (this.videoElement) {
+    this.videoElement.play();
+  }
   this.dispatchEvent('AdStarted');
   callback();
 };
 
 VPAIDCreative.prototype.stopAd = function(callback) {
+  if (this.videoElement) {
+    this.videoElement.pause();
+    this.videoElement.remove();
+  }
   this.dispatchEvent('AdStopped');
   callback();
 };
@@ -42,16 +61,26 @@ VPAIDCreative.prototype.skipAd = function(callback) {
 VPAIDCreative.prototype.resizeAd = function(width, height, viewMode, callback) {
   this.attributes.adWidth = width;
   this.attributes.adHeight = height;
+  if (this.videoElement) {
+    this.videoElement.width = width;
+    this.videoElement.height = height;
+  }
   this.dispatchEvent('AdSizeChange');
   callback();
 };
 
 VPAIDCreative.prototype.pauseAd = function(callback) {
+  if (this.videoElement) {
+    this.videoElement.pause();
+  }
   this.dispatchEvent('AdPaused');
   callback();
 };
 
 VPAIDCreative.prototype.resumeAd = function(callback) {
+  if (this.videoElement) {
+    this.videoElement.play();
+  }
   this.dispatchEvent('AdPlaying');
   callback();
 };
@@ -82,89 +111,7 @@ VPAIDCreative.prototype.dispatchEvent = function(eventName) {
   }
 };
 
-// VPAID Wrapper Implementation
-var VPAIDWrapper = function(VPAIDCreative) {
-  this._creative = VPAIDCreative;
-  if (!this.checkVPAIDInterface(VPAIDCreative)) {
-    console.error('VPAIDCreative doesn\'t conform to the VPAID spec');
-    return;
-  }
-  this.setCallbacksForCreative();
-};
-
-VPAIDWrapper.prototype.checkVPAIDInterface = function(VPAIDCreative) {
-  const requiredFunctions = [
-    'handshakeVersion', 'initAd', 'startAd', 'stopAd', 'skipAd', 'resizeAd',
-    'pauseAd', 'resumeAd', 'expandAd', 'collapseAd', 'subscribe', 'unsubscribe'
-  ];
-
-  for (let func of requiredFunctions) {
-    if (!VPAIDCreative[func] || typeof VPAIDCreative[func] !== 'function') {
-      console.error(`VPAIDCreative is missing or has an invalid function: ${func}`);
-      return false;
-    }
-  }
-  return true;
-};
-
-VPAIDWrapper.prototype.setCallbacksForCreative = function() {
-  var callbacks = {
-    AdStarted: this.onStartAd,
-    AdStopped: this.onStopAd,
-    AdSkipped: this.onSkipAd,
-    AdLoaded: this.onAdLoaded,
-    AdLinearChange: this.onAdLinearChange,
-    AdSizeChange: this.onAdSizeChange,
-    AdExpandedChange: this.onAdExpandedChange,
-    AdSkippableStateChange: this.onAdSkippableStateChange,
-    // Add other callbacks as needed
-  };
-
-  for (let event in callbacks) {
-    if (callbacks.hasOwnProperty(event) && typeof this._creative.subscribe === 'function') {
-      this._creative.subscribe(callbacks[event], event, this);
-    }
-  }
-};
-
-// Example callback functions
-VPAIDWrapper.prototype.onStartAd = function() {
-  console.log('Ad started');
-};
-
-VPAIDWrapper.prototype.onStopAd = function() {
-  console.log('Ad stopped');
-};
-
-VPAIDWrapper.prototype.onSkipAd = function() {
-  console.log('Ad skipped');
-};
-
-VPAIDWrapper.prototype.onAdLoaded = function() {
-  console.log('Ad loaded');
-};
-
-VPAIDWrapper.prototype.onAdLinearChange = function() {
-  console.log('Ad linear change');
-};
-
-VPAIDWrapper.prototype.onAdSizeChange = function() {
-  console.log('Ad size change');
-};
-
-VPAIDWrapper.prototype.onAdExpandedChange = function() {
-  console.log('Ad expanded change');
-};
-
-VPAIDWrapper.prototype.onAdSkippableStateChange = function() {
-  console.log('Ad skippable state change');
-};
-
-// Instantiate the VPAID creative and wrapper
-var vpaidCreative = new VPAIDCreative();
-var vpaidWrapper = new VPAIDWrapper(vpaidCreative);
-
 // Expose the VPAID creative to the global scope
 window.getVPAIDAd = function() {
-  return vpaidCreative;
+  return new VPAIDCreative();
 };
