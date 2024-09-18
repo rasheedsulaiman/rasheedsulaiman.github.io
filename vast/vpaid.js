6615d1e86df93db6be5f09545a050078183938c7
rasheedsulaiman <rasheedsulaiman@users.noreply.github.com>
2024-09-18 16:24:21 -0400

    Update vpaid.js

--- vast/vpaid.js
+++ vast/vpaid.js
@@ -2,19 +2,26 @@
   var VPAIDAd = function() {
       this.eventsCallbacks = {};
       this.adDuration = 30; // Example duration in seconds
+      this.adVolume = 1.0;  // Default volume (100%)
       this.startTime = 0;
       this.isStarted = false;
   };
 
+  // Handshake to verify VPAID version
+  VPAIDAd.prototype.handshakeVersion = function(version) {
+      console.log('handshakeVersion called with version: ' + version);
+      return '2.0';  // Return the supported VPAID version
+  };
+
+  // Initialize the ad
   VPAIDAd.prototype.initAd = function(width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
-      // Initialize the ad with the given parameters
       console.log('Ad initialized with width: ' + width + ', height: ' + height);
       this.startTime = new Date().getTime();
       this.callEvent('AdLoaded');
   };
 
+  // Start the ad experience
   VPAIDAd.prototype.startAd = function() {
-      // Start the ad experience
       this.isStarted = true;
       this.callEvent('AdStarted');
       console.log('Ad started');
@@ -24,8 +31,8 @@
       }, this.adDuration * 1000);
   };
 
+  // Stop the ad
   VPAIDAd.prototype.stopAd = function() {
-      // End the ad
       if (this.isStarted) {
           this.isStarted = false;
           this.callEvent('AdStopped');
@@ -33,35 +40,73 @@
       }
   };
 
+  // Skipping the ad
+  VPAIDAd.prototype.skipAd = function() {
+      console.log('Ad skipped');
+      this.callEvent('AdSkipped');
+  };
+
+  // Resize the ad
+  VPAIDAd.prototype.resizeAd = function(width, height, viewMode) {
+      console.log('Ad resized to width: ' + width + ', height: ' + height);
+      // You can add more functionality to actually handle resizing if necessary
+  };
+
+  // Pause the ad
+  VPAIDAd.prototype.pauseAd = function() {
+      console.log('Ad paused');
+      this.callEvent('AdPaused');
+  };
+
+  // Resume the ad after pause
+  VPAIDAd.prototype.resumeAd = function() {
+      console.log('Ad resumed');
+      this.callEvent('AdResumed');
+  };
+
+  // Expand the ad
   VPAIDAd.prototype.expandAd = function() {
-    // Example of resizing the ad container
-    var adContainer = document.getElementById('adContainer'); // Make sure to reference your ad container
-    if (adContainer) {
-        adContainer.style.width = '100%';
-        adContainer.style.height = '100%';
-    }
-    console.log('expandAd called, resizing container.');
-    // Notify the player that the ad has been expanded
-    this.callEvent('AdExpanded');
+      console.log('Ad expanded');
+      this.callEvent('AdExpanded');
   };
 
-  VPAIDAd.prototype.getAdIcons = function() {
-    // If no ad icons are present, return null or an empty array
-    console.log('getAdIcons called');
-    return null;  // You could also return an empty array or other appropriate structure
-  };
-
+  // Collapse the ad
   VPAIDAd.prototype.collapseAd = function() {
-    // Logic to collapse the ad, if necessary, or a placeholder function
-    console.log('collapseAd called');
-    // Notify the player that the ad has been collapsed
-    this.callEvent('AdCollapsed');
+      console.log('Ad collapsed');
+      this.callEvent('AdCollapsed');
   };
 
+  // Get the ad duration in seconds
+  VPAIDAd.prototype.getAdDuration = function() {
+      console.log('getAdDuration called');
+      return this.adDuration;  // Returns the duration of the ad
+  };
+
+  // Get the current ad volume
+  VPAIDAd.prototype.getAdVolume = function() {
+      console.log('getAdVolume called');
+      return this.adVolume;  // Return the current volume (1.0 = 100%)
+  };
+
+  // Set the ad volume
+  VPAIDAd.prototype.setAdVolume = function(volume) {
+      console.log('setAdVolume called with volume: ' + volume);
+      this.adVolume = volume;
+      this.callEvent('AdVolumeChange');
+  };
+
+  // Get ad icons (if available, return null if not using icons)
+  VPAIDAd.prototype.getAdIcons = function() {
+      console.log('getAdIcons called');
+      return null;  // Return null if no icons are used in the ad
+  };
+
+  // Subscribe to events
   VPAIDAd.prototype.subscribe = function(event, callback) {
       this.eventsCallbacks[event] = callback;
   };
 
+  // Call an event (notify player of ad events)
   VPAIDAd.prototype.callEvent = function(eventType) {
       var callback = this.eventsCallbacks[eventType];
       if (typeof callback === 'function') {
