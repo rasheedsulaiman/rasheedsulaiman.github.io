c13009b588d63d94b9949b2f6d7dd1588bc925a9
rasheedsulaiman <rasheedsulaiman@users.noreply.github.com>
2024-09-18 15:31:25 -0400

    Update vpaid.js

--- vast/vpaid.js
+++ vast/vpaid.js
@@ -1,53 +1,38 @@
 (function() {
   var VPAIDAd = function() {
       this.eventsCallbacks = {};
+      this.adDuration = 30; // Example duration in seconds
+      this.startTime = 0;
       this.isStarted = false;
   };
 
-  // Initialize the ad with the given parameters
   VPAIDAd.prototype.initAd = function(width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
+      // Initialize the ad with the given parameters
       console.log('Ad initialized with width: ' + width + ', height: ' + height);
-      this.injectAdHtml();
+      this.startTime = new Date().getTime();
       this.callEvent('AdLoaded');
   };
 
-  // Inject the entire HTML + script code as-is
-  VPAIDAd.prototype.injectAdHtml = function() {
-      var container = document.createElement('div');
-
-      // Insert the HTML as-is, keeping it intact
-      container.innerHTML = `<div id="ld-7573-2519"></div>
-          <script>
-              (function(w,d,s,i){
-                  w.ldAdInit=w.ldAdInit||[];
-                  w.ldAdInit.push({slot:13217938886688870,size:[0, 0],id:"ld-7573-2519"});
-                  if(!d.getElementById(i)){
-                      var j=d.createElement(s),p=d.getElementsByTagName(s)[0];
-                      j.async=true;
-                      j.src="https://testing.test.decide.co/_js/ajs.js";
-                      j.id=i;
-                      p.parentNode.insertBefore(j,p);
-                  }
-              })(window,document,"script","ld-ajs");
-          </script>`;
-
-      // Append the container to the document body
-      document.body.appendChild(container);
-  };
-
   VPAIDAd.prototype.startAd = function() {
+      // Start the ad experience
       this.isStarted = true;
       this.callEvent('AdStarted');
       console.log('Ad started');
+      // Simulate 30 second ad
+      setTimeout(() => {
+          this.stopAd();
+      }, this.adDuration * 1000);
   };
 
   VPAIDAd.prototype.stopAd = function() {
-      this.isStarted = false;
-      this.callEvent('AdStopped');
-      console.log('Ad stopped');
+      // End the ad
+      if (this.isStarted) {
+          this.isStarted = false;
+          this.callEvent('AdStopped');
+          console.log('Ad stopped');
+      }
   };
 
-  // Callbacks for event subscription and triggering
   VPAIDAd.prototype.subscribe = function(event, callback) {
       this.eventsCallbacks[event] = callback;
   };
