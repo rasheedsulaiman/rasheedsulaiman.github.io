2f3ef3c22cafa36c481b1b49301fcbc0510a0bf1
rasheedsulaiman <rasheedsulaiman@users.noreply.github.com>
2024-09-18 18:17:56 -0400

    add console logs

--- copilot/vpaid-creative.js
+++ copilot/vpaid-creative.js
@@ -16,10 +16,12 @@ var VPAIDCreative = function() {
 };
 
 VPAIDCreative.prototype.handshakeVersion = function(version, callback) {
+  console.log('handshakeVersion called with version:', version);
   callback('2.0');
 };
 
 VPAIDCreative.prototype.initAd = function(width, height, viewMode, desiredBitrate, creativeData, environmentVars, callback) {
+  console.log('initAd called with width:', width, 'height:', height, 'viewMode:', viewMode, 'desiredBitrate:', desiredBitrate);
   this.attributes.adWidth = width;
   this.attributes.adHeight = height;
   this.attributes.adDuration = 30; // Example duration
@@ -41,6 +43,7 @@ VPAIDCreative.prototype.initAd = function(width, height, viewMode, desiredBitrat
 };
 
 VPAIDCreative.prototype.startAd = function(callback) {
+  console.log('startAd called');
   if (this.videoElement) {
     this.videoElement.play();
   }
@@ -51,6 +54,7 @@ VPAIDCreative.prototype.startAd = function(callback) {
 };
 
 VPAIDCreative.prototype.stopAd = function(callback) {
+  console.log('stopAd called');
   if (this.videoElement) {
     this.videoElement.pause();
     this.videoElement.remove();
@@ -62,6 +66,7 @@ VPAIDCreative.prototype.stopAd = function(callback) {
 };
 
 VPAIDCreative.prototype.skipAd = function(callback) {
+  console.log('skipAd called');
   this.dispatchEvent('AdSkipped');
   if (typeof callback === 'function') {
     callback();
@@ -69,6 +74,7 @@ VPAIDCreative.prototype.skipAd = function(callback) {
 };
 
 VPAIDCreative.prototype.resizeAd = function(width, height, viewMode, callback) {
+  console.log('resizeAd called with width:', width, 'height:', height, 'viewMode:', viewMode);
   this.attributes.adWidth = width;
   this.attributes.adHeight = height;
   if (this.videoElement) {
@@ -82,6 +88,7 @@ VPAIDCreative.prototype.resizeAd = function(width, height, viewMode, callback) {
 };
 
 VPAIDCreative.prototype.pauseAd = function(callback) {
+  console.log('pauseAd called');
   if (this.videoElement) {
     this.videoElement.pause();
   }
@@ -92,6 +99,7 @@ VPAIDCreative.prototype.pauseAd = function(callback) {
 };
 
 VPAIDCreative.prototype.resumeAd = function(callback) {
+  console.log('resumeAd called');
   if (this.videoElement) {
     this.videoElement.play();
   }
@@ -102,6 +110,7 @@ VPAIDCreative.prototype.resumeAd = function(callback) {
 };
 
 VPAIDCreative.prototype.expandAd = function(callback) {
+  console.log('expandAd called');
   this.attributes.adExpanded = true;
   this.dispatchEvent('AdExpandedChange');
   if (typeof callback === 'function') {
@@ -110,6 +119,7 @@ VPAIDCreative.prototype.expandAd = function(callback) {
 };
 
 VPAIDCreative.prototype.collapseAd = function(callback) {
+  console.log('collapseAd called');
   this.attributes.adExpanded = false;
   this.dispatchEvent('AdExpandedChange');
   if (typeof callback === 'function') {
@@ -118,14 +128,17 @@ VPAIDCreative.prototype.collapseAd = function(callback) {
 };
 
 VPAIDCreative.prototype.subscribe = function(callback, eventName, context) {
+  console.log('subscribe called for event:', eventName);
   this.eventsCallbacks[eventName] = callback.bind(context);
 };
 
 VPAIDCreative.prototype.unsubscribe = function(eventName) {
+  console.log('unsubscribe called for event:', eventName);
   delete this.eventsCallbacks[eventName];
 };
 
 VPAIDCreative.prototype.dispatchEvent = function(eventName) {
+  console.log('dispatchEvent called for event:', eventName);
   if (this.eventsCallbacks[eventName]) {
     this.eventsCallbacks[eventName]();
   }
@@ -133,56 +146,67 @@ VPAIDCreative.prototype.dispatchEvent = function(eventName) {
 
 // Implement the getAdIcons method
 VPAIDCreative.prototype.getAdIcons = function() {
+  console.log('getAdIcons called');
   return this.attributes.adIcons;
 };
 
 // Implement the getAdDuration method
 VPAIDCreative.prototype.getAdDuration = function() {
+  console.log('getAdDuration called');
   return this.attributes.adDuration;
 };
 
 // Implement the getAdExpanded method
 VPAIDCreative.prototype.getAdExpanded = function() {
+  console.log('getAdExpanded called');
   return this.attributes.adExpanded;
 };
 
 // Implement the getAdSkippableState method
 VPAIDCreative.prototype.getAdSkippableState = function() {
+  console.log('getAdSkippableState called');
   return this.attributes.adSkippableState;
 };
 
 // Implement the getAdRemainingTime method
 VPAIDCreative.prototype.getAdRemainingTime = function() {
+  console.log('getAdRemainingTime called');
   return this.attributes.adRemainingTime;
 };
 
 // Implement the getAdVolume method
 VPAIDCreative.prototype.getAdVolume = function() {
+  console.log('getAdVolume called');
   return this.attributes.adVolume;
 };
 
 // Implement the setAdVolume method
 VPAIDCreative.prototype.setAdVolume = function(volume) {
+  console.log('setAdVolume called with volume:', volume);
   this.attributes.adVolume = volume;
   this.dispatchEvent('AdVolumeChange');
 };
 
 // Implement the getAdWidth method
 VPAIDCreative.prototype.getAdWidth = function() {
+  console.log('getAdWidth called');
   return this.attributes.adWidth;
 };
 
 // Implement the getAdHeight method
 VPAIDCreative.prototype.getAdHeight = function() {
+  console.log('getAdHeight called');
   return this.attributes.adHeight;
 };
 
 // Implement the getAdLinear method
 VPAIDCreative.prototype.getAdLinear = function() {
+  console.log('getAdLinear called');
   return this.attributes.adLinear;
 };
 
 // Expose the VPAID creative to the global scope
 window.getVPAIDAd = function() {
+  console.log('getVPAIDAd called');
   return new VPAIDCreative();
 };
