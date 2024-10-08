ceca4791d4afd7f2b979b1c736f9413f72d1fc69
rasheedsulaiman <rasheedsulaiman@users.noreply.github.com>
2024-10-08 13:25:50 -0500

    Update vpaid.js

--- basil79/vpaid.js
+++ basil79/vpaid.js
@@ -305,7 +305,7 @@ LinearAd.prototype.initAd = function(width, height, viewMode, desiredBitrate, cr
 LinearAd.prototype.startAd = function() {
   console.log('VP > startAd');
 
-  this._videoSlot.play();
+  // this._videoSlot.play();
   //this._videoSlot.load();
   this.onAdStarted();
 };
